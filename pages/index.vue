64f04d54444f5a9a33de6fb0b87e4315992148f9
codanux <codanux7@gmail.com>
2025-05-15 14:03:36 +0300

    form

--- pages/index.vue
+++ pages/index.vue
@@ -58,6 +58,17 @@
             </div>
         </section>
 
+
+        <form @submit="send" style="padding: 10px;">
+            <h1 style="text-align: center;">İletişim Formu</h1>
+            <input type="text" placeholder="Adınız" style="width: 100%; padding: 10px; margin-bottom: 10px;">
+            <input type="text" placeholder="E-posta" style="width: 100%; padding: 10px; margin-bottom: 10px;">
+            <input type="text" placeholder="Telefon" style="width: 100%; padding: 10px; margin-bottom: 10px;">
+            <textarea placeholder="Mesajınız" style="width: 100%; padding: 10px; margin-bottom: 10px;"></textarea>
+            <button style="width: 100%; padding: 10px; background-color: #4CAF50; color: white; border: none; cursor: pointer;">Gönder</button>
+        </form>
+
+
         <section id="contact" class="container" style="height: 30vh;">
            
             <div style="align-content: center; width: 100%; height: 100%;">
@@ -74,6 +85,7 @@
 
         </section>
 
+      
         <footer style="text-align: center;">
             <p>&copy; 2024 Alkan Demir ve İskele. Tüm Hakları Saklıdır.</p>
         </footer>
@@ -99,6 +111,26 @@ onMounted(() => {
     }
 })
 
+async function send(event) {
+    event.preventDefault();
+    const data = await fetch('https://api.alkandemir.com/notification', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: event.target[0].value,
+            email: event.target[1].value,
+            phone: event.target[2].value,
+            message: event.target[3].value
+        })
+    })
+    if (data.status === 201) {
+        alert('Mesajınız başarıyla gönderildi.');
+    } else {
+        alert('Lütfen Email veya telefon numaranızı kontrol edin.');
+    }
+}
 </script>
 
 <style scoped>
