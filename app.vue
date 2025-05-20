3635fe79307d0895b52a0d1d2a0aba962500d9da
codanux <codanux7@gmail.com>
2025-05-20 11:34:19 +0300

    css

--- app.vue
+++ app.vue
@@ -15,4 +15,31 @@
 body {
   font-family: Georgia, 'Times New Roman', Times, serif;
 }
+
+
+.container {
+  display: flex;
+  align-items: center; 
+}
+
+@media (max-width: 800px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container * {
+    width: 100%;
+  }
+}
+.mySlides {display:none;}
+
+.animate {
+  animation: fade 5s;
+}
+
+@keyframes fade {
+  from {opacity: .5} 
+  to {opacity: 1}
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -59,30 +59,5 @@ onMounted(() => {
 
 <style>
 
-.container {
-  display: flex;
-  align-items: center; 
-}
-
-@media (max-width: 800px) {
-  .container {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .container * {
-    width: 100%;
-  }
-}
-.mySlides {display:none;}
-
-.animate {
-  animation: fade 5s;
-}
-
-@keyframes fade {
-  from {opacity: .5} 
-  to {opacity: 1}
-}
 
 </style>
